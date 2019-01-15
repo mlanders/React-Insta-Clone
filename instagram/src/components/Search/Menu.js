fb3097346970aaab0d5acf87471c5f40e24127da
Mike Landers <landers.mike@gmail.com>
2019-01-15 08:43:12 -0800

    minor styling changes

--- instagram/src/components/Search/Menu.js
+++ instagram/src/components/Search/Menu.js
@@ -3,9 +3,9 @@ import React from 'react';
 function Menu(props) {
 	return (
 		<div className="Menu">
-			<i className="far fa-compass" />
-			<i className="far fa-heart" />
-			<i className="far fa-user" />{' '}
+			<i className="far fa-compass fa-2x" />
+			<i className="far fa-heart fa-2x" />
+			<i className="far fa-user fa-2x" />
 		</div>
 	);
 }
